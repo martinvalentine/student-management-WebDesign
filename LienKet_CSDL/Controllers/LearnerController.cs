3e52f9f8122bd2f040f21d13cd5d6f8a08066204
Martin Valentine <nguythequang200@gmail.com>
2023-10-05 02:54:40 +0700

    Add controller for learner

---
+++ LienKet_CSDL/Controllers/LearnerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LienKet_CSDL.Data;
+using LienKet_CSDL.Models;
+
+namespace LienKet_CSDL.Controllers
+{
+    public class LearnerController : Controller
+    {
+            private SchoolContext db;
+            public LearnerController(SchoolContext context)
+            {
+                db = context;
+            }
+            public IActionResult Index()
+            {
+                var learners = db.Learners.Include(m => m.Major).ToList();
+                return View(learners);
+            }
+    }
+}
