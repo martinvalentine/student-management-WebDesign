18ec20cb2e3cbfbfcb95ad39211797c70b5da253
Martin Valentine <nguythequang200@gmail.com>
2023-10-11 01:49:18 +0700

    Lab 4 complete

--- LienKet_CSDL/Controllers/LearnerController.cs
+++ LienKet_CSDL/Controllers/LearnerController.cs
@@ -1,22 +1,147 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LienKet_CSDL.Data;
+using LienKet_CSDL.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using LienKet_CSDL.Data;
-using LienKet_CSDL.Models;
 
 namespace LienKet_CSDL.Controllers
 {
+
     public class LearnerController : Controller
     {
-            private SchoolContext db;
-            public LearnerController(SchoolContext context)
+        private SchoolContext db;
+        public LearnerController(SchoolContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            var learners = db.Learners.Include(m => m.Major).ToList();
+            return View(learners);
+        }
+
+        public IActionResult Create()
+        {
+            var majors = new List<SelectListItem>();
+            foreach (var item in db.Majors)
             {
-                db = context;
+                majors.Add(new SelectListItem
+                {
+                    Text = item.MajorName,
+                    Value = item.MajorID.ToString()
+                });
             }
-            public IActionResult Index()
+            ViewBag.MajorID = majors;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("FirstMidName, LastName, MajorID, EnrollmentDate")] Learner learner)
+        {
+            if(ModelState.IsValid)
             {
-                var learners = db.Learners.Include(m => m.Major).ToList();
-                return View(learners);
+                db.Learners.Add(learner);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
             }
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName");
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (id == null || db.Learners == null)
+            {
+                return NotFound();
+            }
+
+            var learner = db.Learners.Find(id);
+            if(learner == null)
+            {
+                return NotFound();
+            }
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName", learner.MajorID);
+            return View(learner);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id,
+            [Bind("LearnerID,FirstMidName,LastName,MajorID,EnrollmentDate")] Learner learner)
+        {
+            if(id != learner.LearnerID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Update(learner);
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!LearnerExists(learner.LearnerID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.MajorId = new SelectList(db.Majors, "MajorID", "MajorName", learner.MajorID);
+            return View(learner);
+        }
+        private bool LearnerExists(int id)
+        {
+            return (db.Learners?.Any(e => e.LearnerID == id)).GetValueOrDefault();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if(id == null || db.Learners == null)
+            {
+                return NotFound();
+            }
+
+            var learner = db.Learners.Include(l => l.Major).
+                Include(e =>  e.Enrollments).FirstOrDefault(n => n.LearnerID == id);
+
+            if(learner == null)
+            {
+                return NotFound();
+            }
+
+            if(learner.Enrollments.Count() > 0)
+            {
+                return Content("This learner has some enrollments, can't delete!");
+            }
+            return View(learner);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            if(db.Learners == null)
+            {
+                return Problem("Entity set 'Learners' is null.");
+            }
+            var learner = db.Learners.Find(id);
+            if(learner != null)
+            {
+                db.Learners.Remove(learner);
+            }
+
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        
     }
 }
--- LienKet_CSDL/Models/Major.cs
+++ LienKet_CSDL/Models/Major.cs
@@ -4,5 +4,6 @@
     {
         public int MajorID { get; set; }
         public string MajorName { get; set; }
-        public ICollection<Learner> Learners { get; set; }    }
+        public ICollection<Learner> Learners { get; set; }
+    }
 }
